bb0afbfe40ff9dcb41fd28c74284c9a612083af0
Niels V <nvdk@users.noreply.github.com>
2023-03-02 11:28:22 +0100

    update start script
    previously the if statement was not working, fixed by switching to bash as script processor. Also added switches to set up bash strict mode, so it fails on unset variables and exits on any failures

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
-#! /usr/bin/env sh
-set -e
-
-if [ $MODE = "development" ]; then 
+#! /usr/bin/env bash
+set -eu
+if [ ${MODE:-""} == "development" ]; then 
     exec python web.py
 else 
     exec gunicorn -k egg:meinheld#gunicorn_worker -c "$GUNICORN_CONF" "$APP_MODULE"
