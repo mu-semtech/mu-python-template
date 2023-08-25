684533f3cf3f3571991f4b8fe7632bd85231dd00
Sergio Fenoll <sergio@fenoll.be>
2023-08-25 13:42:16 +0200

    fix: install the service provided requirements on startup

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 set -eu
 if [ ${MODE:-""} == "development" ]; then 
-    if [ -f requirements.txt ]; then pip install -r requirements.txt; fi
+    if [ -f /app/requirements.txt ]; then pip install -r /app/requirements.txt; fi
     exec python web.py
 else 
     exec gunicorn -k egg:meinheld#gunicorn_worker -c "$GUNICORN_CONF" "$APP_MODULE"
