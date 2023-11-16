e4c28bee74367c8b0b38c3e0ac78e18424128d6d
Sergio Fenoll <sergio@fenoll.be>
2023-11-16 22:56:29 +0100

    chore: enable live reload in dev mode with gunicorn

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 set -eu
 if [ ${MODE:-""} == "development" ]; then 
     if [ -f /app/requirements.txt ]; then pip install -r /app/requirements.txt; fi
-    exec gunicorn -k egg:meinheld#gunicorn_worker -c "$GUNICORN_CONF" "$APP_MODULE"
+    exec gunicorn -k --reload egg:meinheld#gunicorn_worker -c "$GUNICORN_CONF" "$APP_MODULE"
 else 
     exec gunicorn -k egg:meinheld#gunicorn_worker -c "$GUNICORN_CONF" "$APP_MODULE"
 fi
